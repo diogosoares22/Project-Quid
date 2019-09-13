8b097bbca5863c19897b553a75bc46597854c51c
Viktor Fukala <viktor.fukala@email.cz>
2019-09-13 12:48:51 +0200

    Remove unnecessary print

--- powerTwoAccel.py
+++ powerTwoAccel.py
@@ -202,4 +202,3 @@ if __name__ == "__main__":
             if res.measurements['f'][i]:
                 result += 2**i
         print("Measured {}. The sum is {} mod {}.".format(binar, result, modulo))
-        print(binar)
